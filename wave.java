1f48fecaf65f1583c3252c3153257f7eb1f74af6
ezaan <ezaananjum1601@gmail.com>
2023-08-21 22:37:05 +0530

    Adding code for Substrings of a string

--- wave.java
+++ wave.java
@@ -1,5 +1,27 @@
+import java.util.*;
 public class wave {
     public static void main(String[] args){
-        System.out.println(1%2);
+
+                int nums[]={2,1,-1};
+                int totsum = 0;
+                for (int i : nums) {
+                    totsum += i;
+                }
+                System.out.println("totsum: " + totsum);
+                int num = -1;
+                int leftmost = 0;
+                for (int i = 1; i < nums.length; i++) {
+                    leftmost += nums[i-1];
+                    System.out.println("left: " + leftmost);
+                    System.out.println("nums at i: "+nums[i]);
+                    System.out.print("Right: ");
+                    System.out.println((totsum - leftmost) - nums[i]);
+                    if (leftmost == (totsum - leftmost) - nums[i]) {
+                        num = i;
+                        break;
+                    }
+                }
+        System.out.println(num);
     }
-}
+        }
+
