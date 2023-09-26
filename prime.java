48ab63b08de4f4de0061f044eb0f9bd624ab7ef2
Ezaan <114403253+EzaanA@users.noreply.github.com>
2023-09-26 21:55:44 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class prime {
+public class prime {
+        public static  boolean check(int n){
+            if(n==2){
+                return  true;
+            }
+            for(int i=2;i<=Math.sqrt(n);i++){
+                if(n%i==0){
+                    return  false;
+                }
+            }
+            return  true;
+        }
+        public static  void print_prime(int n){
+            for(int i=2;i<n;i++) {
+                if (check(i)) {
+                    System.out.println(i);
+                }
+            }
+        }
+    public static void main(String[] args) {
+        int n=20;
+        print_prime(n);
+    }
 }
